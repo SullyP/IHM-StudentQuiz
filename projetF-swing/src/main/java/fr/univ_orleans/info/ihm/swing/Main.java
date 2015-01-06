6d1bd8efa0b8ac3012c626244ee29f0727648180
Alexislavie <lavie.alexis@gmail.com>
2015-01-06 18:55:27 +0000

    [swing] Jolie interface pour le swing (cadeau pour Éléonore). - Ajout de substance et trident en conséquence comme dépendances.   Cela ne change rien à la manière de faire du swing. C'est juste un thème   multi-plateforme. J'ai choisi le thème le plus sobre et propre.

--- projetF-swing/src/main/java/fr/univ_orleans/info/ihm/swing/Main.java
+++ projetF-swing/src/main/java/fr/univ_orleans/info/ihm/swing/Main.java
@@ -4,24 +4,40 @@ import fr.univ_orleans.info.ihm.modele.rmi.IModeleService;
 import fr.univ_orleans.info.ihm.swing.vue.Appli;
 import org.apache.log4j.Logger;
 
+import javax.swing.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Main {
-    private static final Logger LOGGER = Logger.getLogger(Main.class.getCanonicalName());
     public static final String HOST = "127.0.0.1";
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getCanonicalName());
 
     private Main() {
     }
 
     public static void main(String[] args) {
-        try {
-            Registry registry = LocateRegistry.getRegistry(HOST, 9345);
-            IModeleService service = (IModeleService) registry.lookup(IModeleService.SERVICE_NAME);
-            Appli application=new Appli("QCM",service);
-        } catch (Exception e) {
-            LOGGER.warn("Remote service exception", e);
-        }
 
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                IModeleService modeleService = null;
+
+                try {
+                    Registry registry = LocateRegistry.getRegistry(HOST, 9345);
+                    modeleService = (IModeleService) registry.lookup(IModeleService.SERVICE_NAME);
+                } catch (Exception e) {
+                    LOGGER.warn("Remote service exception", e);
+                }
+                try {
+                    /* Select the Look and Style you love */
+//                    UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceGeminiLookAndFeel");
+                    UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceGraphiteAquaLookAndFeel");
+//                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception e) {
+                    LOGGER.warn("Look and Feel exception", e);
+                }
+
+                new Appli("QCM", modeleService);
+            }
+        });
     }
 }
